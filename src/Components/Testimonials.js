49939cb949a50aca3dcde72a78fcd7cbafa470cc
Rahul Shivhare <rahulshivhare4444@gmail.com>
2023-06-06 16:35:49 +0530

    second commit added

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -33,10 +33,10 @@ export default function Testimonials(props) {
  
   return ( 
     <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center
-    mt-10 p-10 transition-all duration-700 hover:shadow:xl'>
+    mt-10 p-10 transition-all duration-700 hover:shadow:xl rounded-md'>
       <Card review={reviews[index]}/>
 
-      <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto'>
+      <div className='flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto'>
         <button onClick={leftShiftHandler} className='cursor-pointer hover:text-violet-500'>
             <FiChevronLeft />
         </button>
@@ -45,7 +45,7 @@ export default function Testimonials(props) {
         </button>
       </div>
 
-      <div>
+      <div className='mt-6'>
         <button onClick={surpriseHandler} className='bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>
             Surprise Me
         </button>
